e7dbd9a22d2d9799738401cc4051d947b3c8c6ff
Shaun Houck <31975420+houcks@users.noreply.github.com>
2022-04-05 10:58:26 -0600

    Time: 237 ms (17.27%), Space: 41.6 MB (29.96%) - LeetHub

--- 48-rotate-image/48-rotate-image.cs
+++ 48-rotate-image/48-rotate-image.cs
@@ -1,14 +1,12 @@
 public class Solution {
     public void Rotate(int[][] matrix) {
-        int temp;
-        int end = 0;
+        int temp;     
         for(int i = 0; i < matrix.Length; i++){
-            for(int j = matrix[i].Length - 1; j  >= end; j--){
+            for(int j = matrix[i].Length - 1; j  >= i; j--){
                 temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
-            end++;
         }
         for(int i = 0; i < matrix.Length; i++){
             Array.Reverse(matrix[i]);
